814aab06826da2b87d854a4e5e05efe763415ee0
TejasviD <tejasvidus@yahoo.com>
2022-02-18 23:47:42 +0100

    Initial commit

---
+++ src/components/discord.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import discordText from "../assets/discordText.png";
+import discord from "../assets/discord.png";
+
+export default function discordInvite(){
+    return (
+        // <div className="iconRotate">
+        //   <a href="#">
+        //   <img src={discordText} alt="super1" />
+        //   </a>
+        // </div>
+
+
+
+            <a href="#">
+                <div className="discordLogo" >
+                    <img className="discordImg" src={discord} alt="discordLogo" />
+                </div>
+                <div className="iconRotate">
+                    <img src={discordText} alt="discordText" />
+                </div>    
+            </a>
+        
+        
+      );
+}
