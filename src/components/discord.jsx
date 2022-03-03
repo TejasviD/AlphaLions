cd45341a90f770c0343c6c0eed51aaadd1fac175
Shini_28 <tejasvidus@yahoo.com>
2022-03-03 01:29:51 +0100

    fixed discord icon

--- src/components/discord.jsx
+++ src/components/discord.jsx
@@ -2,23 +2,14 @@ import React from "react";
 import discordText from "../assets/discordText.png";
 import discord from "../assets/discord.png";
 
-export default function discordInvite(){
+export default function discordInvite() {
     return (
-        // <div className="iconRotate">
-        //   <a href="#">
-        //   <img src={discordText} alt="super1" />
-        //   </a>
-        // </div>
+        <a href="#" className="discordLogo-wrapper">
+            <div className="discordLogo">
+                <img className="discordImg" src={discord} alt="discordLogo" />
+            </div>
+            <img className="iconRotate" src={discordText} alt="discordText" />
+        </a>
 
-            <a href="#">
-                <div className="discordLogo" >
-                    <img className="discordImg" src={discord} alt="discordLogo" />
-                </div>
-                <div className="iconRotate">
-                    <img src={discordText} alt="discordText" />
-                </div>    
-            </a>
-        
-        
-      );
+    );
 }
