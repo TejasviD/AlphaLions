76c96ae4a6da8fb5ac78f2fce2c7245291ffe530
Shini_28 <tejasvidus@yahoo.com>
2022-04-11 23:24:55 +0200

    heroku deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Footer from "./components/Footer";
 import Landing from "./components/Landing";
-import Video from "./components/Video";
+// import Video from "./components/Video";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Story from "./components/Story";
@@ -58,8 +58,8 @@ function App() {
     <div data-theme={theme}  className="app-container">
       <Discord />
       <Navbar changeTheme={changeTheme} currentTheme={theme}/>
-      {/* <Landing /> */}
-      <Video />
+      <Landing />
+      {/* <Video /> */}
       <About />
       <Story />
       <Carousel />
